62447bdc66105a5627a371bbde9571e8821790e3
Lidiia Shabaldina <ufenm@student.kit.edu>
2017-11-15 15:08:31 +0100

    Group Changes

---
+++ SeminarTest/src/GroupChanges.java
@@ -0,0 +1,21 @@
+
+public class GroupChanges {
+
+	int groupNr [] = {1, 2, 3, 4};
+	
+	int a = 2;
+	int b = 3;
+	
+	int c = a+b;
+	
+	public void display() {
+		System.out.println("TODO: Jede Gruppe mauss eine kleine Änderung in Code machen");
+		System.out.println ("Gruppe" + groupNr[1] + " macht eine Änderung. Ändere Eingabeparameter a, b und berechne neue Summe c: " + "a+b = " + c);
+		System.out.println ("Gruppe" + groupNr[2] + " macht eine Änderung ");
+		System.out.println ("Gruppe" + groupNr[3] + " macht eine Änderung ");
+		System.out.println ("Gruppe" + groupNr[4] + " macht eine Änderung ");
+		}
+		
+		
+	}
+

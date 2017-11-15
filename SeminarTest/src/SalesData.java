724493e25447b5451634a736269de0b69c28eef1
Lidiia Shabaldina <ufenm@student.kit.edu>
2017-11-15 10:43:51 +0100

    Initial commit

---
+++ SeminarTest/src/SalesData.java
@@ -0,0 +1,9 @@
+
+public class SalesData {
+
+	int data [] = {0, 4, 42};
+	
+	public void display() {
+		System.out.println("TODO: Dein ersten 'git clone' and 'git comit' machen");
+	}
+}
---
+++ SeminarTest/src/SeminarTestMain.java
@@ -0,0 +1,20 @@
+
+public class SeminarTestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+
+	}
+	
+	private static void displayGreeting() {
+		System.out.println("Willkomen zu dem ersten Workshop");
+		System.out.println("Heute geht es um Versionsverwaltung mit Git und GitHub");
+		
+	}
+
+}
